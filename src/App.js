674efa2fcb7d16210cfba791cdfb5d3ca30b97f3
shingymen <kahshing90@hotmail.com>
2020-02-23 20:41:05 +0100

    first-react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Javascript is not impossible to learn 
+        but it will take some time to get comfortable with.</p>
+        <p>That goes for the react programming languages.</p>
+    </div>
+  );
+}
+
+export default App;
